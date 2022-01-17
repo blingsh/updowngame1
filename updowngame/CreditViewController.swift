0d1b7a2635f0f15db0f47f986f33619f8ed88921
SeungHee Seo <https://github.com/blingsh>
2022-01-17 19:09:14 +0900

    my first project!

---
+++ updowngame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  updowngame
+//
+//  Created by Seo on 2022/01/17.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButoon(_ sender: UIButton){
+        dismiss(animated: true,
+                completion: nil)
+    }
+}
